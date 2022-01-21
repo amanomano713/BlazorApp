a41d644afe84efb1a45fbe8829ebf9b4d315baf1
amanomano713 <arodrigues2010@hotmail.com>
2022-01-21 10:08:41 +0100

    add retiro

---
+++ Models/RetiroDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp.Models
+{
+    public class RetiroDTO
+    {
+        public int Retiro { get; set; }
+
+        public string Wallet { get; set; }
+
+        public string Id { get; set; }
+
+        public int Saldo { get; set; }
+    }
+}
