a984ab74690b4c2854cd76eade069de6f342311c
amanomano713 <arodrigues2010@hotmail.com>
2022-01-24 16:34:52 +0100

    changes transfer

--- Models/RetiroDTO.cs
+++ Models/RetiroDTO.cs
@@ -1,12 +1,15 @@
-﻿namespace BlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
 {
     public class RetiroDTO
     {
-        public int Retiro { get; set; }
+        [Required(ErrorMessage = "Monto a retirar es Obligatorio")]
+        public string? Retiro { get; set; }
 
-        public string Wallet { get; set; }
+        public string? Wallet { get; set; }
 
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
         public int Saldo { get; set; }
     }
---
+++ Models/TransferirDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
+{
+    public class TransferirDTO
+    {
+        public string? Saldo { get; set; }
+        public string? Wallet { get; set; }
+        [Required(ErrorMessage = "Monto a transferir es Obligatorio")]
+        public string? Transferir { get; set; }
+        [Required(ErrorMessage = "Código del Afiliado es Obligatorio")]
+        public string? Afiliado { get; set; }
+        public string? Id { get; set; }
+
+    }
+}
