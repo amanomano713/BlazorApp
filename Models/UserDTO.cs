70d6257ebebcee8f8784e7d3c9686856cfdb12ec
amanomano713 <arodrigues2010@hotmail.com>
2022-01-24 18:06:10 +0100

    changes paquetes

--- Models/UserDTO.cs
+++ Models/UserDTO.cs
@@ -4,23 +4,23 @@ namespace BlazorApp.Models
 {
     public class UserDTO
     {
-        public string id { get; set; }
+        public string? id { get; set; }
         [Required(ErrorMessage = "Nombre es Obligatorio")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Nombre no puede tener menos de 6 caracteres y más de 20 caracteres de longitud")]
-        public string name { get; set; }
+        public string? name { get; set; }
         [Required(ErrorMessage = "Apellido es Obligatorio")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Apellido no puede tener menos de 6 caracteres y más de 20 caracteres de longitud")]
-        public string surname { get; set; }
+        public string? surname { get; set; }
         [Required(ErrorMessage = "Fecha de Nacimiento es Obligatorio")]
         public DateTime dateOfbirth { get; set; }
-        public string city { get; set; }
-        public string zipcode { get; set; }
+        public string? city { get; set; }
+        public string? zipcode { get; set; }
 
         [Required(ErrorMessage = "Mobile es Obligatorio")]
-        public string mobile { get; set; }
-
+        public string? mobile { get; set; }
+        
         [Required(ErrorMessage = "Wallet BTC es Obligatorio")]
-        public string wallet { get; set; }
+        public string? wallet { get; set; }
 
         public string email { get; set; }
 
