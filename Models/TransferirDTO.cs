06383b9c852fc60571e806ab4d79678597543653
amanomano713 <arodrigues2010@hotmail.com>
2022-01-25 01:53:31 +0100

    change transfer

--- Models/TransferirDTO.cs
+++ Models/TransferirDTO.cs
@@ -6,8 +6,10 @@ namespace BlazorApp.Models
     {
         public string? Saldo { get; set; }
         public string? Wallet { get; set; }
+
         [Required(ErrorMessage = "Monto a transferir es Obligatorio")]
         public string? Transferir { get; set; }
+
         [Required(ErrorMessage = "Código del Afiliado es Obligatorio")]
         public string? Afiliado { get; set; }
         public string? Id { get; set; }
