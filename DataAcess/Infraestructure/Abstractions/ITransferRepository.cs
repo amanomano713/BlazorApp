f922bb69717e5d22eebf2346bbf8e1dc6498500b
amanomano713 <arodrigues2010@hotmail.com>
2022-01-26 12:42:45 +0100

    CreateTransferCommandHandler

---
+++ DataAcess/Infraestructure/Abstractions/ITransferRepository.cs
@@ -0,0 +1,10 @@
+﻿using BlazorApp.DataAcess.Bases;
+using BlazorApp.Entities.User;
+
+namespace BlazorApp.DataAcess.Infraestructure.Abstractions
+{
+    public interface ITransferRepository : IRepository<Transfer>
+    {
+        Transfer Add(Transfer transfer);
+    }
+}
---
+++ Handlers/User/CreateTransferCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using BlazorApp.DataAcess.Infraestructure.Abstractions;
+using BlazorApp.Entities.User;
+using BlazorApp.Handlers.Commands;
+using MediatR;
+
+namespace BlazorApp.Handlers.User
+{
+    public class CreateTransferCommandHandler : BaseCommandHandler<CreateTransferCommandHandler>, IRequestHandler<CreateTransferCommand, Transfer>
+    {
+        private readonly ITransferRepository _transferRepository;
+        public CreateTransferCommandHandler(
+            IMapper mapper,
+            ITransferRepository transferRepository) : base(mapper)
+        {
+            _transferRepository = transferRepository ?? throw new ArgumentNullException(nameof(transferRepository));
+        }
+
+        public async Task<Transfer> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+            var transfer = _mapper.Map<Transfer>(request);
+
+            _transferRepository.Add(transfer);
+
+            await _transferRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+            return transfer;
+        }
+    }
+}
