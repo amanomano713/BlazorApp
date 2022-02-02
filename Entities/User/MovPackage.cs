849f6cb2257c13e5a4f4a66a86e3520b516b356c
arodrigues2014 <arodrigues2010@hotmail.com>
2022-02-02 22:38:17 +0100

    add movpackages

--- Entities/User/MovPackage.cs
+++ Entities/User/MovPackage.cs
@@ -1,17 +1,24 @@
 ﻿using BlazorApp.DataAcess.Bases;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorApp.Entities.User
 {
     [Table("MovPackage")]
     [Index(nameof(Id), IsUnique = true)]
-    public class MovPackage 
+    public class MovPackage
     {
         public string? Id { get; set; }
+        [Required]
+        public string? CodigoId { get; set; }
+        [Required]
         public string? CodPackage { get; set; }
+        [Required]
         public new DateTime? CreatedDate { get; set; }
+        [Required]
         public float Interes { get; set; }
+        [Required]
         public float Porcentaje { get; set; }
     }
 }
