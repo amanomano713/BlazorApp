25b772dd30baea9a407965ac2fbd8dee0dda981c
arodrigues2014 <arodrigues2010@hotmail.com>
2022-02-02 22:03:06 +0100

    add IMovPackageDataQueries

---
+++ DataAcess/Infraestructure/Abstractions/IMovPackageDataQueries.cs
@@ -0,0 +1,10 @@
+﻿using BlazorApp.Entities.User;
+
+namespace BlazorApp.DataAcess.Infraestructure.Abstractions
+{
+    public interface IMovPackageDataQueries
+    {
+        public Task<IEnumerable<MovPackage>> GetAfiliadoDataAsync(string id);
+
+    }
+}
