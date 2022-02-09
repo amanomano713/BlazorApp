a3467f26b29baf3dd3899af66d7b60c0f3290f8f
amanomano713 <arodrigues2010@hotmail.com>
2022-02-09 23:07:52 +0100

    Fix XPO

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using BlazorApp.Encryptor;
 using BlazorApp.Models;
+using DevExpress.Xpo;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorApp.Services
@@ -9,12 +10,14 @@ namespace BlazorApp.Services
     {
         SignInModel User { get; }
         Task Login(SignInModel model);
-        Task<String> GetItem();
+        Task<string> GetItem();
 
+        //Task<IQueryable<MovPackage>> Get();
     }
-
+    
     public class AccountService : IAccountService
     {
+        private readonly UnitOfWork _readUnitOfWork;
         private readonly IMapper _mapper;
         private NavigationManager _navigationManager;
         private ILocalStorageService _localStorageService;
@@ -29,15 +32,37 @@ namespace BlazorApp.Services
             IMapper mapper,
             NavigationManager navigationManager,
             ILocalStorageService localStorageService,
-            IEncryptor IEncryptor
+            IEncryptor IEncryptor,
+            UnitOfWork readUnitOfWork
         ) {
             _mapper = mapper;
             _navigationManager = navigationManager;
             _localStorageService = localStorageService;
             _IEncryptor = IEncryptor;
+            _readUnitOfWork = readUnitOfWork;
 
         }
 
+        //public Task<IQueryable<MovPackage>> Get()
+        //{
+
+        //    Session MainSession = new Session();
+
+        //    XPQuery<MovPackage> customersQuery = new XPQuery<MovPackage>(MainSession);
+
+        //    var customers = from c in customersQuery
+        //                    select c;
+
+
+        //    foreach (MovPackage customer in customers)
+        //        Console.WriteLine(customer.Id);
+
+        //    var resul = _readUnitOfWork.Query<MovPackage>().Select(m => m.IdAfiliado);
+
+        //    var query = (IQueryable<MovPackage>)_readUnitOfWork.Query<MovPackage>();
+
+        //    return Task.FromResult(query);
+        //}
 
         public async Task Login(SignInModel model)
         {
