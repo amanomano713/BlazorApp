2628faa4dff94d1862f06c0ae2cf2685dbc258db
arodrigues2014 <arodrigues2010@hotmail.com>
2022-02-13 18:52:11 +0100

    changes packages

--- Handlers/User/CreatePackagesCommandHandler.cs
+++ Handlers/User/CreatePackagesCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using BlazorApp.Data.EF;
 using BlazorApp.DataAcess.Infraestructure.Abstractions;
 using BlazorApp.Entities.User;
 using BlazorApp.Handlers.Commands;
@@ -11,11 +12,14 @@ namespace BlazorApp.Handlers.User
     public class CreatePackagesCommandHandler : BaseCommandHandler<CreatePackagesCommandHandler>, IRequestHandler<CreatePackagesCommand, Packages>
     {
         private readonly IPackagesRepository _packagesDataRepository;
+        private readonly Context _context;
         public CreatePackagesCommandHandler(
             IMapper mapper,
+             Context context,
             ILogger<CreatePackagesCommandHandler> logger,
             IPackagesRepository packagesDataRepository) : base(mapper,logger)
         {
+            _context = context; 
             _packagesDataRepository = packagesDataRepository ?? throw new ArgumentNullException(nameof(packagesDataRepository));
         }
 
@@ -23,14 +27,44 @@ namespace BlazorApp.Handlers.User
         {
             var packages = _mapper.Map<Packages>(request);
 
-            packages = _packagesDataRepository.Add(packages);
-            
-            if (packages==null)
+            var movPackage = new MovPackage
             {
-                throw new MyGOfitException(ExceptionType.Unknown, ExceptionRepository.NotFound, ExceptionEntity.Unknown, $"packages not found");
+                IdAfiliado = request.IdAfiliado,
+                DateCreated = DateTime.Now,
+                MontoTransferido = 0,
+                IdPackage = 0,
+                CodPackage = "Crypto",
+                Porcentaje = 0,
+                MontoPackage = request.Monto,
+                Interes = 0,
+                MontoRetiro = 0
+            };
+
+            await using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    _context.Packages.Add(packages);
+                    _context.MovPackage.Add(movPackage);
+                    await _context.SaveEntitiesAsync(cancellationToken);
+                    await transaction.CommitAsync();
+                }
+                catch (Exception e)
+                {
+                    await transaction.RollbackAsync();
+                    throw new MyGOfitException(ExceptionType.Unknown, ExceptionRepository.NotFound, ExceptionEntity.Unknown, $"transfer not found");
+                }
 
             }
-            await _packagesDataRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+            //packages = _packagesDataRepository.Add(packages);
+
+            //if (packages==null)
+            //{
+            //    throw new MyGOfitException(ExceptionType.Unknown, ExceptionRepository.NotFound, ExceptionEntity.Unknown, $"packages not found");
+
+            //}
+            //await _packagesDataRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
 
             _logger.LogInformation("Create Packages this is a information message...");
 
